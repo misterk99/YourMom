ea50214c31726cf6c891c083704e1b12149d314e
Kyle <kyle_michiels@hotmail.com>
2020-01-31 22:28:19 +0100

    Added Github Plugin
    Description

---
+++ Assets/ActionBase.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionBase : MonoBehaviour
+{
+    enum E_ArmHeight { LOW,MIDDLE,HIGH};
+    [SerializeField]
+    E_ArmHeight armHeight;
+    bool thrust;
+    [SerializeField]
+    string instult;
+
+}
