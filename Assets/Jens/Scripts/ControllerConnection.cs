07904fe7019741565088a1afe4bef9d0fc1608fb
Jens Petter <j.petter111@gmail.com>
2020-02-01 10:06:30 +0100

    Day 1 work pushed.

---
+++ Assets/Jens/Scripts/ControllerConnection.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerConnection : MonoBehaviour
+{
+    public GameObject m_PlayerPrefab;
+    void Start()
+    {
+        string[] joysticks = Input.GetJoystickNames();
+
+        int controllers = 2;
+        for (int i = 0; i < joysticks.Length; i++)
+        {
+            if (joysticks[i].Contains("Xbox"))
+            {
+                controllers++;
+            }
+        }
+
+        for (int i = 0; i < controllers; i++)
+        {
+            GameObject obj;
+            if (controllers > 1)
+            {
+                if (i == 0)
+                {
+                    obj = Instantiate(m_PlayerPrefab, Vector3.zero, Quaternion.identity);
+                }
+                else
+                {
+                    obj = Instantiate(m_PlayerPrefab, new Vector3(50, 0, 0), Quaternion.identity);
+                }
+            }
+            else
+            {
+                obj = Instantiate(m_PlayerPrefab, Vector3.zero, Quaternion.identity);
+            }
+            
+
+            if (controllers > 1)
+            {
+                if (i == 0)
+                {
+                    obj.GetComponentInChildren<Camera>().rect = new Rect(0, 0, 0.5f, 1);
+                }
+                else
+                {
+                    obj.GetComponentInChildren<Camera>().rect = new Rect(0.5f, 0, 0.5f, 1);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Jens/Scripts/MoveTo.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveTo : MonoBehaviour
+{
+    private Vector3 destination;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            GetComponent<NavMeshAgent>().destination = transform.position + transform.forward;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            GetComponent<NavMeshAgent>().destination = transform.position + -transform.forward;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, 1, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, -1, 0);
+        }
+    }
+}
