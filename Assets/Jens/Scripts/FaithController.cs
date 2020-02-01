2559d85ac9718e5ab614457a319dc1742fce4a47
Jens Petter <j.petter111@gmail.com>
2020-02-01 11:15:41 +0100

    Player and VR player currency.

---
+++ Assets/Jens/Scripts/FaithController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaithController : MonoBehaviour
+{
+    private int m_Faith;
+
+    void Start()
+    {
+        m_Faith = 0;
+        StartCoroutine(GetFaithFromTemples());
+    }
+
+    IEnumerator GetFaithFromTemples()
+    {
+        Temple[] temples = FindObjectsOfType<Temple>();
+
+        for (int i = 0; i < temples.Length; i++)
+        {
+            m_Faith += temples[i].m_CurrencyOverTime;
+        }
+        Debug.Log(m_Faith);
+        yield return new WaitForSeconds(1);
+        StartCoroutine(GetFaithFromTemples());
+    }
+}
