368efd9f11fc6abf9bd179ec1574d566ccb29fa1
Kyle <kyle_michiels@hotmail.com>
2020-02-01 08:00:12 +0100

    Added SteamVR

--- Assets/ActionBase.cs
+++ Assets/ActionBase.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class ActionBase : MonoBehaviour
 {
+    [SerializeField]
     enum E_ArmHeight { LOW,MIDDLE,HIGH};
     [SerializeField]
     E_ArmHeight armHeight;
+    [SerializeField]
     bool thrust;
     [SerializeField]
     string instult;
