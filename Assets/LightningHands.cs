0cb61888221ce2ba100c17da5f576708320e62f0
Kyle <kyle_michiels@hotmail.com>
2020-02-01 14:11:46 +0100

    Changed TestLevel

--- Assets/LightningHands.cs
+++ Assets/LightningHands.cs
@@ -10,15 +10,54 @@ public class LightningHands : MonoBehaviour
     DigitalRuby.LightningBolt.LightningBoltScript lightningBolt;
     [SerializeField]
     GameObject amingTarget;
+    [SerializeField]
+    public Valve.VR.SteamVR_Action_Boolean grabPinchAction = Valve.VR.SteamVR_Input.GetAction<Valve.VR.SteamVR_Action_Boolean>("GrabPinch");
+    [SerializeField]
+    public Valve.VR.SteamVR_Input_Sources handType;
+    [SerializeField]
+    bool canShoot = true;
+    [SerializeField]
+    LayerMask mask;
 
     // Start is called before the first frame update
     // Update is called once per frame
+
+    private void Start()
+    {
+        hand = GetComponent<Valve.VR.InteractionSystem.Hand>();
+    }
     void Update()
     {
         RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.forward * 5000, out hit))
+        if (Physics.Raycast(transform.position, transform.forward, out hit, Mathf.Infinity,mask))
         {
-            amingTarget.transform.position = hit.point + new Vector3(0, 0, 0.1f);
+            if (hit.transform.gameObject.layer == LayerMask.NameToLayer("Ground"))
+            {
+                Debug.DrawRay(transform.position, transform.forward * hit.distance, Color.cyan);
+
+                amingTarget.transform.position = hit.point + new Vector3(0, 0.01f, 0);
+            }
+            else
+            {
+                Debug.Log(hit.transform.gameObject.layer);
+            }
         }
+        if (canShoot)
+        {
+            if (grabPinchAction.GetStateDown(handType))
+            {
+                lightningBolt.gameObject.SetActive(true);
+                canShoot = false;
+                StartCoroutine(Reset());
+            }
+        }
+    }
+
+    IEnumerator Reset()
+    {
+        yield return new WaitForSeconds(1.0f);
+        lightningBolt.gameObject.SetActive(false);
+        yield return new WaitForSeconds(0.5f);
+        canShoot = true;
     }
 }
